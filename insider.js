b98a9c488d1be9810011e74c6828aca90ad7e443
Alvin Julian <alvin.julian@outlook.com>
2024-07-26 22:26:41 +0700

    feat: card will expire
    cleanup and add feat card will expire parser
    add phone number as identifier
    fix has surprise product

--- insider.js
+++ insider.js
@@ -32,13 +32,11 @@ function sendDataToInsider(data) {
 }
 
 function transformData(eventName, eventProperties) {
-    let _skio = _skioData;
-    let _insider = {};
-
     // Map the fields from Skio to Insider
     let insEventName = eventNameConverter(eventName);
     let insTimestamp = moment().toISOString();
-    let insEmail = eventProperties.email;
+    let insEmail = eventProperties.email ? eventProperties.email : '';
+    let insPhoneNumber = eventProperties.phone ? eventProperties.phone : null;
 
 
     if(insEventName === 'skio_billing_attempt_failed') {
@@ -46,7 +44,8 @@ function transformData(eventName, eventProperties) {
             "skip_hook": false,
             "users": [{
                 "identifiers": {
-                    "email": insEmail
+                    "email": insEmail,
+                    "phone_number": insPhoneNumber
                 },
                 "attributes": {
                     "skio_subscription_id": eventProperties.subscriptionId,
@@ -65,7 +64,7 @@ function transformData(eventName, eventProperties) {
                     "skio_currency_symbol": eventProperties.currencySymbol,
                     "skio_total_subscription_price": eventProperties.total,
                     "skio_has_surprise_discount": eventProperties.hasSurpriseDiscount,
-                    "skio_has_surprise_product": JSON.stringify(eventProperties.hasSurpriseProduct),
+                    "skio_has_surprise_product": eventProperties.hasSurpriseProduct,
 
                     // Skio Address Fields
                     "skio_address_first_line": eventProperties.shippingAddress.address1,
@@ -84,6 +83,55 @@ function transformData(eventName, eventProperties) {
                                 "error_message": eventProperties.errorMessage,
                                 "error_code": eventProperties.errorCode,
                                 "number_of_attempts": eventProperties.numberOfFailedAttempts
+                            }
+                        }
+                    }
+                ]
+            }]
+        }
+    } else if(insEventName === 'skio_card_will_expire') {
+        return {
+            "skip_hook": false,
+            "users": [{
+                "identifiers": {
+                    "email": insEmail,
+                    "phone_number": insPhoneNumber
+                },
+                "attributes": {
+                    "skio_subscription_id": eventProperties.subscriptionId,
+                    "skio_subscription_status": eventProperties.status,
+                    "skio_delivery_price": eventProperties.deliveryPrice,
+                    "skio_cancelled_at": eventProperties.cancelledAt,
+                    "skio_next_billing_at": eventProperties.nextBillingAt,
+                    "skio_billing_interval": eventProperties.billingInterval,
+                    "skio_billing_interval_count": eventProperties.intervalCount,
+                    "skio_manage_subscription_url": eventProperties.manageSubscriptionUrl,
+                    "skio_is_prepaid": eventProperties.isPrepaid,
+                    "skio_qa_token": eventProperties.qaToken,
+                    "skio_subscription_lines": JSON.stringify(eventProperties.subscriptionLines),
+                    "skio_box_subscription_lines": JSON.stringify(eventProperties.boxSubscriptionLines),
+                    "skio_currency_code": eventProperties.currencyCode,
+                    "skio_currency_symbol": eventProperties.currencySymbol,
+                    "skio_total_subscription_price": eventProperties.total,
+                    "skio_has_surprise_discount": eventProperties.hasSurpriseDiscount,
+                    "skio_has_surprise_product": eventProperties.hasSurpriseProduct,
+
+                    // Skio Address Fields
+                    "skio_address_first_line": eventProperties.shippingAddress.address1,
+                    "skio_address_second_line": eventProperties.shippingAddress.address2,
+                    "skio_address_city": eventProperties.shippingAddress.city,
+                    "skio_address_country": eventProperties.shippingAddress.country,
+                    "skio_address_province": eventProperties.shippingAddress.province,
+                    "skio_address_zip": eventProperties.shippingAddress.zip,
+                },
+                "events": [
+                    {
+                        "event_name": insEventName,
+                        "timestamp": insTimestamp,
+                        "event_params": {
+                            "custom": {
+                                "last_four_digits": eventProperties.paymentMethodLastDigits,
+                                "days_until_expiry": eventProperties.daysUntilExpiration
                             }
                         }
                     }
